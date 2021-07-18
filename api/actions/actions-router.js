50b9e91088bc9d598552835ad9dc4f2f5d0776df
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-07-17 21:28:34 -0400

    initialization

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,15 @@
-// Write your "actions" router here!
+const express = require('express');
+const router = express.Router();
+const Actions = require('./actions-model');
+
+router.get('/', async (req, res, next) => {
+    await Actions.get()
+        .then(actions => {
+            actions
+            ? res.status(200).json(actions)
+            : res.status(404).json({ message: "No Actions Found" })
+        })
+        .catch(err => next(err))
+});
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,15 @@
-// Write your "projects" router here!
+const express = require('express');
+const router = express.Router();
+const Projects = require('./projects-model');
+
+router.get('/', async (req, res, next) => {
+    await Projects.get()
+        .then(projects => {
+            projects
+            ? res.status(200).json(projects)
+            : res.status(404).json({ message: "No Projects Found" })
+        })
+        .catch(err => next(err))
+});
+
+module.exports = router;
