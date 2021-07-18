9cd313f82a3e8ced8d9fc6c0cfb1d60b61656b43
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2021-07-17 22:45:28 -0400

    using dotenv; endpoints for actions & projects

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -12,4 +12,49 @@ router.get('/', async (req, res, next) => {
         .catch(err => next(err))
 });
 
+router.get('/:id', async (req, res, next) => {
+    const { id } = req.params;
+    await Actions.get(id)
+        .then(action => {
+            action
+            ? res.status(200).json(action)
+            : res.status(404).json({ message: `No Action Found With ID ${id}` })
+        })
+        .catch(err => next(err))
+});
+
+router.post('/', async (req, res, next) => {
+    const newPost = req.body;
+    await Actions.insert(newPost)
+        .then(newAction => {
+            newAction
+            ? res.status(201).json(newAction)
+            : res.status(400).json({ message: "bad request" })
+        })
+        .catch(err => next(err))
+});
+
+router.put('/:id', async (req, res, next) => {
+    const { id } = req.params;
+    const changes = req.body;
+    await Actions.update(id, changes)
+        .then(updatedActions => {
+            updatedActions
+            ? res.status(200).json(updatedActions)
+            : res.status(404).json({ message: `Action with ID ${id} not found` })
+        })
+        .catch(err => next(err))
+});
+
+router.delete('/:id', async (req, res, next) => {
+    const { id } = req.params;
+    await Actions.remove(id)
+        .then(delAct => {
+            delAct
+            ? res.status(200).json(delAct)
+            : res.status(404).json({ message: `Action with ID ${id} not found` })
+        })
+        .catch(err => next(err))
+});
+
 module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -12,4 +12,49 @@ router.get('/', async (req, res, next) => {
         .catch(err => next(err))
 });
 
+router.get('/:id', async (req, res, next) => {
+    const { id } = req.params;
+    await Projects.get(id)
+        .then(project => {
+            project
+            ? res.status(200).json(project)
+            : res.status(404).json({ message: `No Project Found With ID ${id}` })
+        })
+        .catch(err => next(err))
+});
+
+router.post('/', async (req, res, next) => {
+    const newPost = req.body;
+    await Projects.insert(newPost)
+        .then(newProject => {
+            newProject
+            ? res.status(201).json(newProject)
+            : res.status(400).json({ message: "bad request" })
+        })
+        .catch(err => next(err))
+});
+
+router.put('/:id', async (req, res, next) => {
+    const { id } = req.params;
+    const changes = req.body;
+    await Projects.update(id, changes)
+        .then(updatedProj => {
+            updatedProj
+            ? res.status(200).json(updatedProj)
+            : res.status(404).json({ message: `Project ID ${id} not found` })
+        })
+        .catch(err => next(err))
+});
+
+router.delete('/:id', async (req, res, next) => {
+    const { id } = req.params;
+    await Projects.remove(id)
+        .then(delProj => {
+            delProj
+            ? res.status(200).json(delProj)
+            : res.status(404).json({ message: `Project with ID ${id} not found` })
+        })
+        .catch(err => next(err))
+});
+
 module.exports = router;
